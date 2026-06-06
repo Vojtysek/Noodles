73004077da726936caf4b7f7035cfb025bf991cb
Vojtěch Döme <ojta@post.cz>
2026-06-06 10:34:47 +0200

    fix: remove unnecessary whitespace in ProjectsPage component

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -205,7 +205,7 @@ export default function ProjectsPage() {
             )}
           </div>
         </div>
-      </div>
+      </div> 
 
       {/* Project grid */}
       <div data-joyride="projekty-list" className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
